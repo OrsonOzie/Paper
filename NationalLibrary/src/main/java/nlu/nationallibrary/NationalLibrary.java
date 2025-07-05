18738d0f30fa8ed20a7181bdcd20290710ba6b2a
Buwule Ronald <orsonozie@gmail.com>
2025-07-05 14:53:59 +0300

    Add files via upload

---
+++ NationalLibrary/src/main/java/nlu/nationallibrary/NationalLibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package nlu.nationallibrary;
+
+/**
+ *
+ * @author USER
+ */
+public class NationalLibrary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
