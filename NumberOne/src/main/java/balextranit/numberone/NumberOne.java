6be882fe38cf1ad8e0c0283803958712204c9941
Buwule Ronald <orsonozie@gmail.com>
2025-07-05 16:21:23 +0300

    Add files via upload

---
+++ NumberOne/src/main/java/balextranit/numberone/NumberOne.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package balextranit.numberone;
+
+/**
+ *
+ * @author USER
+ */
+public class NumberOne {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
